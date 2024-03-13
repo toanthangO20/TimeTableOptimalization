004185ff38edaf065454c92a6fc2a3b49efadf74
toanthangO20 <tailieuonthidaihoccuathang@gmail.com>
2024-03-13 11:40:35 +0700

    init backtracking

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int n, m; // Số lớp học và phòng học
 int t[N], g[N], s[N]; // Số tiết học, giáo viên được phân công dạy lớp và số học sinh của lớp
 int c[M]; // Số chỗ ngồi của mỗi phòng học
 int above; //Số lớp có chỗ ngồi không vượt quá chỗ ngồi các phòng
-int maxRoom; //Số chỗ ngồi nhiều nhất của một lớp
+int maxRoom; //Số chỗ ngồi nhiều nhất của một phòng học
 
 bool markTeacher[105][65]; // markTeacher[i][j] : Giáo viên i đã dạy tiết j
 bool markRoom[M][65]; //markRoom[i][j] : Phòng i đã bận vào tiết j
@@ -21,13 +21,10 @@ int pmax[N], rmax[N]; // pmax[i], rmax[i]: Ghi nhận cấu hình tốt nhất c
 
 int f; //Biến để sử dụng nhánh và cận
 
-int debug;
-
-void input()
+void input() // Đọc dữ liệu đầu vào
 {
     freopen("input.txt", "r", stdin);
     cin >> n >> m;
-    above = n;
     maxRoom = 0;
     for (int i = 1; i <= n; i++)
     {
@@ -36,19 +33,13 @@ void input()
     for (int i = 1; i <= m; i++)
     {
         cin >> c[i];
-        maxRoom = maxRoom > c[i] ? maxRoom : c[i];
     }
-    for (int i = 1; i <= n; i++)
-    {
-        if(s[i] > maxRoom) above--;
-    }
-    f = 0;
-    //cout << "above: " << above << endl;
 }
 
-void setInitValue()
+void setInitValue() //Khởi tạo giá trị cho các cấu trúc dữ liệu
 {
     countClass = 0;
+    maxClass = 0;
     for (int i = 1; i <= 100; i++)
     {
         for (int j = 1; j <= 60; j++)
@@ -63,26 +54,32 @@ void setInitValue()
             markRoom[i][j] = false;
         }
     }
-    maxClass = 0;
+    above = n;
+    for (int i = 1; i <= m; i++)
+    {
+        maxRoom = maxRoom > c[i] ? maxRoom : c[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if(s[i] > maxRoom) above--;
+    }
 }
 
-bool check(int k, int v1, int v2)
+bool check(int k, int v1, int v2) //Kiểm tra xem v1, v2 có hợp lệ không
 {
-    if (maxClass == above) return false;
-    if (v1 == 0 && v2 == 0) return true;
-    if((v1 == 0 && v2 > 0) || (v1 > 0 && v2 == 0)) return false;
-    // markTeacher[g[k]][v1]
-    for (int i = 1; i <= t[k]; i++)
+    if (maxClass == above) return false; //Nếu maxClass đạt đến số lớp tối đa có thể xếp chỗ ngồi thì dừng
+    if((v1 == 0 && v2 > 0) || (v1 > 0 && v2 == 0)) return false; //Kiểm tra ràng buộc (1)
+    for (int i = 1; i <= t[k]; i++) //Kiểm tra ràng buộc (2)
     {
         if (markTeacher[g[k]][v1+i-1]) return false;
         if (markRoom[v2][v1+i-1]) return false;
     }
-    if (s[k] > c[v2]) return false;
-    if ((60 - v1) % 6 + 1 < t[k]) return false;
+    if (s[k] > c[v2]) return false; //Kiểm tra ràng buộc (3)
+    if ((60 - v1) % 6 + 1 < t[k]) return false; //Kiểm tra ràng buộc (4)
     return true;
 }
 
-void updateBestSolution()
+void updateBestSolution() //Ghi nhận cấu hình, cập nhật lời giải tối ưu
 {
     if (countClass > maxClass)
     {
@@ -95,7 +92,7 @@ void updateBestSolution()
     }
 }
 
-void printBestSolution()
+void printBestSolution() //In ra lời giải tối ưu
 {
     cout << maxClass << endl;
     for (int i = 1; i <= n; i++)
@@ -119,36 +116,32 @@ void trySolution(int k)
             {
                 p[k] = v1;
                 r[k] = v2;
-                if (v1 > 0)
+                if (v1 > 0) //Cập nhật các cấu trúc dữ liệu đi kèm nếu xếp được lịch cho lớp k
                 {
+                    countClass++;
                     for (int i = 1; i <= t[k]; i++)
                     {
                         markTeacher[g[k]][v1+i-1] = true;
                         markRoom[v2][v1+i-1] = true;
                     }
-                    countClass++;
-
-                    //if (debug == 99 || debug == 100) cout << "Da cong countClass" << endl;
                 }
                 if (k == n)
                 {
                     updateBestSolution();
-                    //setInitValue();
                 }
                 else
                 {
-                    f = countClass + n - k;
+                    f = countClass + n - k; //Số lớp xếp được tối đa của cấu hình đang xét
                     if (f > maxClass) trySolution(k+1);
                 }
-                if (v1 > 0)
+                if (v1 > 0) //Khôi phục các cấu trúc dữ liệu đi kèm nếu xếp được lịch cho lớp k
                 {
+                    countClass--;
                     for (int i = 1; i <= t[k]; i++)
                     {
                         markTeacher[g[k]][v1+i-1] = false;
                         markRoom[v2][v1+i-1] = false;
                     }
-                    countClass--;
-                    //if (debug == 99 || debug == 100) cout << "Da tru countClass" << endl;
                 }
             }
         }
@@ -159,7 +152,6 @@ int main()
 {
     input();
     setInitValue();
-    debug = 0;
     trySolution(1);
     printBestSolution();
     return 0;
